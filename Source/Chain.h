c8da96f08dd3f953a02a03cc95fff408dd5e734a
xxxAIRINxxx <xl1138@gmail.com>
2016-02-07 00:38:51 +0900

    Fix version variable name

--- Source/Chain.h
+++ Source/Chain.h
@@ -9,10 +9,10 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for Chain.
-FOUNDATION_EXPORT double GCDVersionNumber;
+FOUNDATION_EXPORT double ChainVersionNumber;
 
 //! Project version string for Chain.
-FOUNDATION_EXPORT const unsigned char GCDVersionString[];
+FOUNDATION_EXPORT const unsigned char ChainVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Chain/PublicHeader.h>
 
