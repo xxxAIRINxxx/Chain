ae84ad6438b1f7496015eb9b6a5559fb6034830f
xxxAIRINxxx <xl1138@gmail.com>
2019-03-26 19:13:09 +0900

    Migrate to Swift 5

--- Sources/Chain.swift
+++ Sources/Chain.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public typealias Closure = ((Any?) -> Any?)
-public typealias StartClosure = ((Void) -> Any?)
+public typealias StartClosure = (() -> Any?)
 public typealias CompletionClosure = ((Any?) -> Void)
 
 // MARK: - Chain Class
@@ -57,51 +57,51 @@ extension Chain {
 
 // MARK: - Static Functions
 
-extension Chain {
+public extension Chain {
     
-    public static func main(_ closure: @escaping StartClosure) -> Chain {
+    static func main(_ closure: @escaping StartClosure) -> Chain {
         return Chain(Queue.main, nil, { _ in return closure() })
     }
     
-    public static func background(_ closure: @escaping StartClosure) -> Chain {
+    static func background(_ closure: @escaping StartClosure) -> Chain {
         return Chain(Queue.background, nil, { _ in return closure() })
     }
     
-    public static func userInteractive(_ closure: @escaping StartClosure) -> Chain {
+    static func userInteractive(_ closure: @escaping StartClosure) -> Chain {
         return Chain(Queue.userInteractive, nil, { _ in return closure() })
     }
     
-    public static func userInitiated(_ closure: @escaping StartClosure) -> Chain {
+    static func userInitiated(_ closure: @escaping StartClosure) -> Chain {
         return Chain(Queue.userInitiated, nil, { _ in return closure() })
     }
     
-    public static func utility(_ closure: @escaping StartClosure) -> Chain {
+    static func utility(_ closure: @escaping StartClosure) -> Chain {
         return Chain(Queue.utility, nil, { _ in return closure() })
     }
     
-    public static func onDefault(_ closure: @escaping StartClosure) -> Chain {
+    static func onDefault(_ closure: @escaping StartClosure) -> Chain {
         return Chain(Queue.default, nil, { _ in return closure() })
     }
   
-    public static func custom(_ queue: DispatchQueue, _ closure: @escaping StartClosure) -> Chain {
+    static func custom(_ queue: DispatchQueue, _ closure: @escaping StartClosure) -> Chain {
         return Chain(Queue.custom(queue: queue), nil, { _ in return closure() })
     }
     
-    public static func after(_ queue: Queue = Queue.background, seconds: Double, _ closure: @escaping StartClosure) -> Chain {
+    static func after(_ queue: Queue = Queue.background, seconds: Double, _ closure: @escaping StartClosure) -> Chain {
         return Chain(queue, nil) { _ in
             Chain.waitBlock(seconds)()
             return closure()
         }
     }
     
-    public static func wait(_ queue: Queue = Queue.background, seconds: Double, _ closure: StartClosure) -> Chain {
+    static func wait(_ queue: Queue = Queue.background, seconds: Double, _ closure: StartClosure) -> Chain {
         return Chain(queue, nil) { _ in
             Chain.waitBlock(seconds)()
             return nil
         }
     }
     
-    fileprivate static func waitBlock(_ seconds: Double) -> ((Void) -> Void) {
+    fileprivate static func waitBlock(_ seconds: Double) -> (() -> Void) {
         return {
             let nanoSeconds = Int64(seconds * Double(NSEC_PER_SEC))
             let time = DispatchTime.now() + Double(nanoSeconds) / Double(NSEC_PER_SEC)
@@ -114,44 +114,44 @@ extension Chain {
 
 // MARK: - Instance Functions
 
-extension Chain {
+public extension Chain {
     
-    public final func main(_ closure: @escaping Closure) -> Chain {
+    final func main(_ closure: @escaping Closure) -> Chain {
         return Chain(Queue.main, self, closure)
     }
     
-    public final func background(_ closure: @escaping Closure) -> Chain {
+    final func background(_ closure: @escaping Closure) -> Chain {
         return Chain(Queue.background, self, closure)
     }
     
-    public final func userInteractive(_ closure: @escaping Closure) -> Chain {
+    final func userInteractive(_ closure: @escaping Closure) -> Chain {
         return Chain(Queue.userInteractive, self, closure)
     }
     
-    public final func userInitiated(_ closure: @escaping Closure) -> Chain {
+    final func userInitiated(_ closure: @escaping Closure) -> Chain {
         return Chain(Queue.userInitiated, self, closure)
     }
     
-    public final func utility(_ closure: @escaping Closure) -> Chain {
+    final func utility(_ closure: @escaping Closure) -> Chain {
         return Chain(Queue.utility, self, closure)
     }
     
-    public final func onDefault(_ closure: @escaping Closure) -> Chain {
+    final func onDefault(_ closure: @escaping Closure) -> Chain {
         return Chain(Queue.default, self, closure)
     }
     
-    public final func custom(_ queue: DispatchQueue, _ closure: @escaping Closure) -> Chain {
+    final func custom(_ queue: DispatchQueue, _ closure: @escaping Closure) -> Chain {
         return Chain(Queue.custom(queue: queue), self, closure)
     }
     
-    public final func after(_ queue: Queue = Queue.background, seconds: Double, _ closure: @escaping Closure) -> Chain {
+    final func after(_ queue: Queue = Queue.background, seconds: Double, _ closure: @escaping Closure) -> Chain {
         return Chain(queue, self) { result in
             Chain.waitBlock(seconds)()
             return closure(result)
         }
     }
     
-    public final func wait(_ queue: Queue = Queue.background, seconds: Double) -> Chain {
+    final func wait(_ queue: Queue = Queue.background, seconds: Double) -> Chain {
         return Chain(queue, self) { result in
             Chain.waitBlock(seconds)()
             return result
